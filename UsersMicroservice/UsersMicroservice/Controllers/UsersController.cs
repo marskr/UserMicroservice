d93fcf535adcbf6dab0ef5b5be100741b71c4b77
marskr <marskr@opoczta.pl>
2018-03-30 00:46:58 +0200

    API GET first step

---
+++ UsersMicroservice/UsersMicroservice/Controllers/UsersController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsersMicroservice.Data;
+using Newtonsoft.Json;
+
+namespace UsersMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/users
+        [HttpGet]
+        public string Get()
+        {
+            var json = JsonConvert.SerializeObject(_context.Users);
+
+            return json;
+        }
+
+        // GET api/users/{id}
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+
+            var settings = _context.Users.Find(id);
+            var json = JsonConvert.SerializeObject(settings);
+            if (settings == null)
+            {
+                return "{}";
+            }
+
+            return json;
+        }
+
+        // POST api/users
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _context.Users.Remove(_context.Users.Find(id));
+            _context.SaveChanges();
+        }
+    }
+}
--- UsersMicroservice/UsersMicroservice/Startup.cs
+++ UsersMicroservice/UsersMicroservice/Startup.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 using UsersMicroservice.Data;
 
 namespace UsersMicroservice
@@ -31,6 +26,12 @@ namespace UsersMicroservice
                                                 options.UseSqlServer(connStr));
 
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "MSUser", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,9 +40,30 @@ namespace UsersMicroservice
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseMvc();
+            app.UseStaticFiles();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.  
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.  
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MSUser");
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                          name: "default",
+                          template: "{controller=Todo}/{action=Index}/{id?}");
+            });
         }
     }
 }
